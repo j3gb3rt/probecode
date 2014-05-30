a46e8c780b7ea3a220b52f2a045ff7c812d113a7
Jonathan Egbert <jegbert.eggy@gmail.com>
2014-05-30 05:16:04 +0000

    working gpio code

--- windware/windware.c
+++ windware/windware.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-#include <libserialport.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include <time.h>
 
 #define DEBUG
 #define TRACE
 
+static volatile int tick;
+
+void ticker(void) {
+	tick++;
+}
+
+
 int main(int argc, char* argv) {
-  struct sp_port **portlist;
-  struct sp_port *port;
-  enum sp_return ret;
-  int index;
+	wiringPiSetup();
+	pinMode(0, INPUT);
+	pullUpDnControl(0, PUD_OFF);
+	wiringPiISR(0, INT_EDGE_RISING, &ticker);
+	
+	tick = 0;
+	time_t last_time;
+	time_t curr_time;
+	last_time = time(NULL);
+	double speed;
 
-  ret = sp_list_ports(&portlist);
-
-  for (index = 0; portlist[index] != NULL; index++) {
-    if(strcmp(sp_get_port_name(portlist[index]), "/dev/ttyUSB0") == 0) {
-      sp_copy_port(portlist[index], &port);
-      break;
-    }
-  }
-  if (port != NULL) {
-    struct sp_port_config *config;
-
-    printf("%s\n", sp_get_port_name(port));
-    ret = sp_new_config(&config);
-    printf("%p\n", port);
-    printf("%p\n", config);
-    printf("%i\n", ret);
-    ret = sp_get_config(port, config);
-    printf("%i\n", ret);
-
-    ret = sp_open(port, SP_MODE_READ);
-    printf("%i\n", ret);
-  }
+	while (1) {
+		curr_time = time(NULL);
+		if (difftime(curr_time, last_time) >= 1) {
+			last_time = curr_time;
+			speed = tick * 2.5;
+			printf( "%i, %.2f, %.2f\n",tick, 2.5, speed);
+			tick = 0;
+		}
+	}
+	return 0;
 }
